39d450c98c80b7cbb03054d959048402331e0425
Mollika-deb <108429309+Mollika-deb@users.noreply.github.com>
2022-09-03 14:53:00 +0600

    gategory details added

--- js/index.js
+++ js/index.js
@@ -1,38 +1,81 @@
-const loadCetagories = ()=>{
+const loadCategories = ()=>{
     const url = `https://openapi.programming-hero.com/api/news/categories`
     fetch(url)
     .then(res => res.json())
-        .then(data => displayCetagories(data.data.news_category))
+        .then(data => displayCategories(data.data.news_category))
 }
 
-const displayCetagories = (cetagories) =>{
-    const cetagoriesContainer = document.getElementById('categories-container');
-    cetagories.forEach(cetagory => {
-        console.log(cetagory);
-         const cetagoriesDiv = document.createElement('div');
-         cetagoriesDiv.classList.add('heading');
-         cetagoriesDiv.innerHTML=`
+const displayCategories = (categories) =>{
+    const categoriesContainer = document.getElementById('categories-container');
+    categories.forEach(category => {
+        console.log(category);
+         const categoriesDiv = document.createElement('div');
+         categoriesDiv.classList.add('heading');
+         categoriesDiv.innerHTML=`
 
-             <a onclick = "loadCetagoryDetail(${cetagory.category_id})" class="nav-link" href="#" >${cetagory.category_name}</a>
+             <a onclick = "loadCategoryDetail(${category.category_id})" class="nav-link" href="#" >${category.category_name}</a>
         `;
-          cetagoriesContainer.appendChild(cetagoriesDiv);
+          categoriesContainer.appendChild(categoriesDiv);
     });
 
 }
 
- const loadCetagoryDetail = (category_id) =>{
+ const loadCategoryDetail = (category_id) =>{
   
      const url = ` https://openapi.programming-hero.com/api/news/category/0${category_id} `;
      //console.log(url);
 
      fetch (url)
      .then(res => res.json())
-     .then(data => console.log(data.data[0]));  
+     .then(data => displayCategoriesDetails(data.data));  
 
- }
+ };
 
- const displayCetagoriesDetail = (cetagory) =>{
-    
- }
+//   const displayCategoriesDetails = (category) =>{
+//       const detailContainer = document.getElementById('detail-container');
+//      const  categoryDiv = document.createElement('div');
+//      categoryDiv.classList.add('div');
+//      categoryDiv.innerHTML = `
+//          <div class="row g-0">
+//          <div class="col-md-4">
+//              <img src="..." class="img-fluid rounded-start" alt="...">
+//          </div>
+//          <div class="col-md-8">
+//              <div class="card-body">
+//                 <h5 class="card-title">Card title</h5>
+//                 <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional
+//                     content. This content is a little bit longer.</p>
+//                 <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>             </div>
+//          </div>
+//      </div>
+//     `
+//      detailContainer.appendChild(categoryDiv);
+//  }
 
-loadCetagories();
+
+  const displayCategoriesDetails = (category) =>{
+      const detailContainer = document.getElementById('detail-container');
+      detailContainer.innerHTML = '';
+     category.forEach(details => {
+         const categoryDiv = document.createElement('div');
+      categoryDiv.classList.add('div');
+      categoryDiv.innerHTML = `
+          <div class="row g-0">
+         <div class="col-md-4">
+              <img src="${details.image_url}" class="img-fluid rounded-start" alt="...">
+         </div>
+         <div class="col-md-8">
+              <div class="card-body">
+              <h5 class="card-title">${details.title}</h5>      
+                         <p class="card-text">${details.details}</p>
+                 <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            </div>
+         </div>
+    </div>
+    `
+     detailContainer.appendChild(categoryDiv)
+     })
+  };
+ 
+
+loadCategories();
