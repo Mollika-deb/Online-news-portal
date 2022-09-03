1ad1879d1fecc8f853754aa33120d9f91c58d1e7
Mollika-deb <108429309+Mollika-deb@users.noreply.github.com>
2022-09-03 16:02:28 +0600

    all details added

--- js/index.js
+++ js/index.js
@@ -31,27 +31,6 @@ const displayCategories = (categories) =>{
 
  };
 
-//   const displayCategoriesDetails = (category) =>{
-//       const detailContainer = document.getElementById('detail-container');
-//      const  categoryDiv = document.createElement('div');
-//      categoryDiv.classList.add('div');
-//      categoryDiv.innerHTML = `
-//          <div class="row g-0">
-//          <div class="col-md-4">
-//              <img src="..." class="img-fluid rounded-start" alt="...">
-//          </div>
-//          <div class="col-md-8">
-//              <div class="card-body">
-//                 <h5 class="card-title">Card title</h5>
-//                 <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional
-//                     content. This content is a little bit longer.</p>
-//                 <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>             </div>
-//          </div>
-//      </div>
-//     `
-//      detailContainer.appendChild(categoryDiv);
-//  }
-
 
   const displayCategoriesDetails = (category) =>{
       const detailContainer = document.getElementById('detail-container');
@@ -70,11 +49,21 @@ const displayCategories = (categories) =>{
                          <p class="card-text">${details.details}</p>
                  <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
                  <div>
+                    <div>
                     <img src="${details.author.img}" " rounded-start" alt="...">
+                    </div>
                     <div>
                     <h4>${details.author.name}</h4>
                     <p>${details.author.published_date}</p>
                     </div>
+                    <div>
+                    <i class="fa-regular fa-eye"></i>
+                    <h3>${details.total_view}</h3>
+                    </div>
+                    <div>
+                    <i class="fa-sharp fa-solid fa-arrow-right"></i>
+                    </div>
+
                  </div>
             </div>
          </div>
@@ -86,4 +75,4 @@ const displayCategories = (categories) =>{
   };
  
 
-loadCategories();
+loadCategories('');
