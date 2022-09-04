d6385d3c59bde3b0c0f9bd29513fd9fc423f8fee
Mollika-deb <108429309+Mollika-deb@users.noreply.github.com>
2022-09-04 10:28:25 +0600

    spinner added

--- js/index.js
+++ js/index.js
@@ -2,10 +2,12 @@ const loadCategories = ()=>{
     const url = `https://openapi.programming-hero.com/api/news/categories`
     fetch(url)
     .then(res => res.json())
-        .then(data => displayCategories(data.data.news_category))
+    .then(data => displayCategories(data.data.news_category))
+    .catch(error => console.log(error))
 }
 
 const displayCategories = (categories) =>{
+  toggoleSpinner(true);
     const categoriesContainer = document.getElementById('categories-container');
     categories.forEach(category => {
         console.log(category);
@@ -27,7 +29,8 @@ const displayCategories = (categories) =>{
 
      fetch (url)
      .then(res => res.json())
-     .then(data => displayCategoriesDetails(data.data));  
+     .then(data => displayCategoriesDetails(data.data)); 
+     
 
  };
 
@@ -40,9 +43,18 @@ const displayCategories = (categories) =>{
             noData.classList.remove('d-none');
         }
       }
+    //  const count = document.getElementById('count-details');
+
+    //  const countDiv = document.createElement('p');
+    //  countDiv.classList.add('countAns');
+    //  countDiv.innerHTML = `
+    //     <p>${category.length} news is found</p>
+    //    `
+    //  count.appendChild(countDiv);
+
     //   if(categoryLength===0){
     //     const length = "There is no data";
-    //   }
+    //  }
     //   else{
     //     const length = categoryLength;
     //   }
@@ -52,7 +64,7 @@ const displayCategories = (categories) =>{
          const categoryDiv = document.createElement('div');
       categoryDiv.classList.add('div');
       categoryDiv.innerHTML = `
-            <p>${categoryLength} news is found</p>
+            <p>${category.length} news is found</p>
           <div class="row g-0 ">
          <div class="col-md-4">
               <img src="${details.image_url}" class="img-fluid rounded-start" alt="...">
@@ -81,7 +93,7 @@ const displayCategories = (categories) =>{
                     </div>
                     </div>
                     <div>
-                    <i class="fa-sharp fa-solid fa-arrow-right"></i>
+                    <i onclick="loadNewsDetails(${details._id})" class="fa-sharp fa-solid fa-arrow-right"  data-bs-toggle="modal" data-bs-target="#newsModal"></i>
 
                     </div>
 
@@ -93,17 +105,52 @@ const displayCategories = (categories) =>{
     `
      detailContainer.appendChild(categoryDiv)
      })
+     toggoleSpinner(false);
   };
 
   const loadNewsDetails = (_id) =>{
-    //   const url = `https://openapi.programming-hero.com/api/news/${_id}`
-    //   .then(res => res.json())
-    //   .then(data => console.log(data))
+      const url = `https://openapi.programming-hero.com/api/news/${details._id}`
+       .then(res => res.json())
+       .then(data => console.log(data))
     console.log(_id);
 
   }
 
+  const blog = () =>{
+      const clickBlog = document.getElementById('click-blog');
+      clickBlog.innerHTML = '';
+      const blogDiv = document.createElement('div');
+      blogDiv.classList.add('blogAns');
+      blogDiv.innerHTML=`
+      <h3>Whats are the difference between var , let & const ?</h3>
+      <p>The scope of a let variable is block scope. The scope of a const variable is block scope. var declarations are globally scoped or function scoped</p>
+      </br>
+      <h3>Whats are the difference between arrow function & regular function ?</h3>
+      <p>The first and most obvious difference between arrow functions and regular functions is their syntax. Not only do they look different, but arrow functions also provide an implicit return shorthand and allow parenthesis around a single argument to be omitted.</p>
+      </br>
+      <h3>Whats are the difference between Map , Filter , Find & forEach method ?</h3>
+      <p>.forEach(), is used to execute the same code on every element in an array but does not change the array and it returns undefined..map() executes the same code on every element in an array and returns a new array with the updated elements..filter() checks every element in an array to see if it meets a certain criteria and returns a new array with the elements that return truthy for the criteria.</p>
+      `
+      clickBlog.appendChild(blogDiv);
+  }
+
+   const toggoleSpinner = (isLoading) => {
+    const loadSection = document.getElementById('loader');
+    if(isLoading){
+     loadSection.classList.remove('d-none');
+     }
+     else{
+      loadSection.classList.add('d-none');
+     }
+   }
+
+
+
+     
+  
+
+
 
  
 
-loadCategories('');
+loadCategories('8');
